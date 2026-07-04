aebbcadb8871dcdd1b192748687049f1d1c2444e
gaspar200309 <202201712@gmail.com>
2026-07-04 19:56:11 -0400

    fix: changes to vide

--- src/vide.jsx
+++ src/vide.jsx
@@ -1,7 +1,8 @@
 function Video({ video }) {
   return (
     <div>
-
+<h1>{video.title} Juan de mecanico</h1>
+Me da hmbre
       <a href={video.url}>
         <h3>{video.title}</h3>
         <p>{video.description}</p>
